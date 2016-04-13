abed7dfe8203b0e6394fd642f3f64b0284cd4aeb
dkittle <don@kittle.ca>
2016-04-13 14:07:31 -0400

    fixed unit test

--- src/test/scala/chapter1/UserSpec.scala
+++ src/test/scala/chapter1/UserSpec.scala
@@ -1,12 +1,14 @@
 package chapter1
 
+import org.scalatest.{Matchers, FlatSpec}
+
 class UserSpec extends FlatSpec with Matchers {
 
   "A user's name" should "be extracted" in {
     def extract(user: User): String = user match {
       case FreeUser(name) => s"Hello ${name}"
     }
-    extract(new FreeUser("Alfred")) should be ("Alfred")
+    extract(new FreeUser("Alfred")) should be ("Hello Alfred")
   }
 
 }
